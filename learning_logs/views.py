2daa2a0f75e2bb2b139e2ff0c1dc2d2b4fd1096d
readyshen <readyshen@gmail.com>
2024-07-09 01:01:03 +0800

    增加Topic条目的页面new_topic.html，同一个views.new_topic()方法处理两种页面请求方式

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from learning_logs.models import Topic
+from .forms import TopicForm
 
 
 # Create your views here.
@@ -22,3 +23,16 @@ def topic(request, topic_id):
     # context里包含topic以及entry集合
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
