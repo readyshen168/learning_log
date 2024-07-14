2428408528aecd0d478d917e3e9d70df70cb46ea
readyshen <readyshen@gmail.com>
2024-07-14 16:07:44 +0800

    更改login.html的存储路径到accounts下面的templates/registration文件夹

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('home/', views.index2, name='index2')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index2(request):
+    return render(request, 'accounts/index2.html')
